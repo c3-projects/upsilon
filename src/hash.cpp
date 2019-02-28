6eefbeb3160a3bec6c9ad7af88de7478ed97a1c2
Cyclic3 <cyclic3.git@gmail.com>
2019-02-28 21:19:41 +0000

    Added initial impl of partial_hash

--- src/hash.cpp
+++ src/hash.cpp
@@ -5,6 +5,32 @@
 
 #define C3_UPSILON_DEF_HASH_BOTAN(CLASS_NAME, ALG, BOTAN_HASH_NAME) \
   thread_local static auto CLASS_NAME##_impl = Botan::HashFunction::create(BOTAN_HASH_NAME); \
+  class CLASS_NAME##_partial : public partial_hash { \
+  public: \
+    static constexpr auto props = get_hash_properties<ALG>(); \
+    static constexpr auto static_props = props; \
+  private: \
+    std::unique_ptr<Botan::HashFunction> hf = Botan::HashFunction::create(BOTAN_HASH_NAME); \
+    nu::data salt; \
+  public: \
+    void process(nu::data_const_ref input) override { hf->process(input.data(), input.size()); } \
+    void finish(nu::data_ref output) override { \
+      hf->process(salt.data(), salt.size()); \
+      if (output.size() == props.max_output) \
+        hf->final(output.data()); \
+      else if (static_cast<size_t>(output.size()) > props.max_output) \
+        throw std::range_error("Too many bytes requested from hash"); \
+      else { \
+        std::array<uint8_t, props.max_output> tmp_output; \
+        hf->final(tmp_output.data()); \
+        std::copy(tmp_output.begin(), tmp_output.begin() + output.size(), output.begin()); \
+      } \
+    } \
+    void reset() override { hf->clear(); } \
+  public: \
+    CLASS_NAME##_partial() : salt{} {} \
+    CLASS_NAME##_partial(nu::data salt) : salt{std::move(salt)} {} \
+  }; \
   class CLASS_NAME : public hash_function { \
   public: \
     static constexpr auto props = get_hash_properties<ALG>(); \
@@ -36,10 +62,10 @@
       } \
     } \
     std::unique_ptr<partial_hash> begin_hash() const override { \
-      throw std::runtime_error("Not implemented"); \
+      return std::make_unique<CLASS_NAME##_partial>(); \
     } \
-    std::unique_ptr<partial_hash> begin_hash(nu::data_const_ref) const override{ \
-      throw std::runtime_error("Not implemented"); \
+    std::unique_ptr<partial_hash> begin_hash(nu::data_const_ref salt) const override{ \
+      return std::make_unique<CLASS_NAME##_partial>(nu::data(salt.begin(), salt.end())); \
     } \
     const hash_properties* properties() const noexcept override { return &static_props; } \
   }; \
