44db058d2a2e7bd4b82e776de14761709ffc54ff
Cyclic3 <cyclic3.git@gmail.com>
2019-02-28 21:01:44 +0000

    Finished laserproof alg

--- include/c3/upsilon/pow.hpp
+++ include/c3/upsilon/pow.hpp
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "c3/upsilon/hash.hpp"
+#include "c3/nu/bigint.hpp"
 
 namespace c3::upsilon {
   template<size_t HashSize>
@@ -17,22 +18,50 @@ namespace c3::upsilon {
 
   class pow_verifier {
   public:
-    /// Returns a BE integer that describes the difficulty of the work done
-    ///
-    /// Shoudl return an empty array on failure
-    virtual nu::data difficulty(nu::data_const_ref hashed_data,
-                                nu::data_const_ref proof) const noexcept = 0;
+    /// Returns a integer that describes the difficulty of the work done
+    virtual nu::biguint difficulty(nu::data_const_ref hashed_data,
+                                   nu::data_const_ref proof) const noexcept = 0;
 
   public:
     virtual ~pow_verifier() = default;
   };
 
-  class laserproof_verifier : public pow_verifier {
-  private:
-    hasher h;
+  class pow_creator {
+  public:
+    /// Returns a integer that describes the difficulty of the work done
+    virtual nu::data work(nu::data_const_ref hashed_data,
+                          nu::biguint threshold) const noexcept = 0;
 
   public:
-    nu::data difficulty(nu::data_const_ref hashed_data,
-                        nu::data_const_ref proof) const noexcept override;
+    virtual ~pow_creator() = default;
+  };
+
+  class laserproof : public pow_verifier, public pow_creator {
+  private:
+    hasher h;
+    size_t lookback_len = 4096;
+    size_t backstride_len = 8192;
+    /// The final hash is XORed into a single byte, and compared to threshold
+    ///
+    /// If it is lesser than it, we stop
+    ///
+    /// Therefore there is a theshold / 256 possibility of stopping
+    uint8_t threshold = 128;
+
+  public:
+    static constexpr size_t proof_len = 32;
+
+  private:
+    nu::biguint check_one(nu::data_const_ref hashed_data, nu::data_const_ref proof) const;
+    bool check_complete(nu::data_const_ref buf, nu::data_const_ref proof) const;
+
+  public:
+    nu::biguint difficulty(nu::data_const_ref hashed_data,
+                           nu::data_const_ref proof) const noexcept override;
+    nu::data work(nu::data_const_ref hashed_data,
+                  nu::biguint threshold) const noexcept override;
+
+  public:
+    laserproof(hash_algorithm hash_alg) : h{get_hasher(hash_alg)} {}
   };
 }
--- src/pow/laserproof.cpp
+++ src/pow/laserproof.cpp
@@ -7,23 +7,50 @@
 //!
 
 namespace c3::upsilon {
-  class laserproof_verifier : public pow_verifier {
-  public:
-    static constexpr size_t proof_len = 32;
+  bool laserproof::check_complete(nu::data_const_ref buf, nu::data_const_ref proof) const {
+    auto final_block_start = buf.begin() + (buf.size() % backstride_len);
+    auto first_block_start = buf.end() - 1 - backstride_len;
 
-  public:
-    nu::data difficulty(nu::data_const_ref hashed_data,
-                        nu::data_const_ref proof) const noexcept override {
-      try {
-        if (proof.size() != proof_len)
-          throw std::runtime_error("Proof size incorrect");
+    auto ph = h.begin_hash(proof);
 
-        // Perform the expansion
+    for (auto iter = first_block_start; iter != final_block_start; iter -= buf.size())
+      ph->process({&*iter, static_cast<nu::data_const_ref::index_type>(backstride_len)});
 
-      }
-      catch (...) {
-        return {};
-      }
+    auto hash = ph->finish();
+
+    uint8_t result = 0;
+    for (auto i : hash)
+      result ^= i;
+
+    return result < threshold;
+  }
+
+  nu::biguint laserproof::check_one(nu::data_const_ref hashed_data, nu::data_const_ref proof) const {
+    nu::data buf;
+    do {
+      // Add a work block
+      size_t next_lookback_len = std::min(buf.size(), lookback_len);
+      size_t next_lookback_offset = buf.size() - next_lookback_len;
+      nu::data_const_ref next_lookback{buf.data() + next_lookback_offset,
+                                       static_cast<nu::data_const_ref::index_type>(next_lookback_len)};
+      auto work_block = h.get_hash(next_lookback).value;
+      buf.insert(buf.end(), work_block.begin(), work_block.end());
     }
-  };
+    while (!check_complete(buf, proof));
+
+    return buf.size();
+  }
+
+  nu::biguint laserproof::difficulty(nu::data_const_ref hashed_data,
+                                     nu::data_const_ref proof) const noexcept {
+    try {
+      if (proof.size() != proof_len)
+        throw std::runtime_error("Proof size incorrect");
+
+      return check_one(hashed_data, proof);
+    }
+    catch (...) {
+      return 0;
+    }
+  }
 }
